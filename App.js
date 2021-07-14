d0657fb030c29d9a2d2976d81032958f6162dc8e
Jashvitha-Reddy <76088172+Jashvitha-Reddy@users.noreply.github.com>
2021-07-14 15:37:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignupLoginScreen from './screens/SignupLoginScreen';
+import ExchangeScreen from './screens/ExchangeScreen';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/Settings Screen';
+import customSideBarMenu from './component/customSideBarMenu';
+import {createBottomTabNavigator, createTabNavigator} from 'react-navigation-tabs';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
+
+export default function App() {
+  return (
+    
+      <SignupLoginScreen/>
+   
+  );
+}
+
+
+const Navigator=createBottomTabNavigator({
+  ExchangeScreen:{screen:ExchangeScreen},
+  HomeScreen:{screen:HomeScreen},
+})
+
+const AppDrawerNavigator=createDrawerNavigator({
+  Home:{
+    screen:createTabNavigator
+  },
+  Settings:{
+    screen:SettingsScreen
+  },
+  {
+    contentComponent:customSideBarMenu
+  },
+  {
+    intialRouteName:"Home"
+  }
+})
+
+
+// const styles = StyleSheet.create({
+  // container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  // },
+// });
